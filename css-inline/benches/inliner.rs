38b6a3940e28973fd7849ecd1ad67e61122b7505
Dmitry Dygalo <dmitry@dygalo.dev>
2023-08-17 14:12:15 +0200

    perf: simplify benchmark

--- css-inline/benches/inliner.rs
+++ css-inline/benches/inliner.rs
@@ -1,6 +1,5 @@
 use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
-use css_inline::inline;
-use once_cell::sync::Lazy;
+use css_inline::inline_to;
 use std::fs;
 
 #[derive(serde::Deserialize, Debug)]
@@ -9,17 +8,21 @@ struct Benchmark {
     html: String,
 }
 
-static BENCHMARKS: Lazy<Vec<Benchmark>> = Lazy::new(|| {
+fn inlining(c: &mut Criterion) {
     let benchmarks_str =
         fs::read_to_string("../benchmarks/benchmarks.json").expect("Failed to load benchmarks");
-    serde_json::from_str(&benchmarks_str).expect("Failed to load benchmarks")
-});
-
-fn inlining(c: &mut Criterion) {
-    for benchmark in BENCHMARKS.iter() {
+    let benchmarks: Vec<Benchmark> =
+        serde_json::from_str(&benchmarks_str).expect("Failed to load benchmarks");
+    for benchmark in benchmarks.iter() {
         let html = black_box(&benchmark.html);
         c.bench_function(&benchmark.name, |b| {
-            b.iter(|| inline(html).expect("Inlining failed"))
+            let mut output = Vec::with_capacity(
+                (html.len() as f64 * 1.5).min(usize::MAX as f64).round() as usize,
+            );
+            b.iter(|| {
+                inline_to(html, &mut output).expect("Inlining failed");
+                output.clear();
+            })
         });
     }
 }
