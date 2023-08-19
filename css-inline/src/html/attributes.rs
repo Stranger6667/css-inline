f4338f7ae7078f0e9efcf3329ed12af8c895841c
Dmitry Dygalo <dmitry@dygalo.dev>
2023-08-19 21:40:34 +0200

    docs: Update Bloom filter docs

--- css-inline/src/html/attributes.rs
+++ css-inline/src/html/attributes.rs
@@ -9,9 +9,14 @@ use std::{
     hash::{Hash, Hasher},
 };
 
+/// Class attribute value wrapper that provides a way to make fast class lookups.
 #[derive(Debug)]
 pub(crate) struct Class {
     pub(crate) value: StrTendril,
+    // In CSS selector matching, checking an element's class is frequent. 
+    // Given that classes are often specific, most elements won't have the checked class. 
+    // Leveraging this, we use a Bloom filter for a quick initial check. 
+    // If positive, we do an actual check. This two-tier approach ensures fewer actual checks on class attributes
     pub(crate) cache: CountingBloomFilter<BloomStorageBool>,
 }
 
@@ -19,6 +24,7 @@ static SELECTOR_WHITESPACE: &[char] = &[' ', '\t', '\n', '\r', '\x0C'];
 
 impl Class {
     fn new(value: StrTendril) -> Class {
+        // Build a Bloom filter for all element's classes
         let mut cache = CountingBloomFilter::<BloomStorageBool>::new();
         for class in value.split(SELECTOR_WHITESPACE) {
             let hash = hash_class_name(class.as_bytes());
@@ -27,12 +33,16 @@ impl Class {
         Class { value, cache }
     }
 
+    // Check whether this element MAY have the given class.
+    // If positive, then the class is probably there, and in this case we need to perform the actual check.
+    // Otherwise, the element does not have this class.
     #[inline]
     pub(crate) fn might_have_class(&self, name: &[u8]) -> bool {
         let hash = hash_class_name(name);
         self.cache.might_contain_hash(hash)
     }
 
+    // Manually check whether the class atteibute value contains the given class.
     #[inline]
     fn has_class_impl(&self, name: &[u8], case_sensitivity: CaseSensitivity) -> bool {
         for class in self.value.split(SELECTOR_WHITESPACE) {
@@ -73,6 +83,7 @@ pub(crate) fn hash_class_name(name: &[u8]) -> u32 {
 pub(crate) struct Attributes {
     /// Attribute names and their respective values.
     pub(crate) map: BTreeMap<QualName, StrTendril>,
+    /// The 'class' attribute value is separated for performance reasons.
     pub(crate) class: Option<Class>,
 }
 
