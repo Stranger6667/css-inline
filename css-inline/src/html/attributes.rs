2d2017b8031dff073cbb28a9edda973b79021f68
Dmitry Dygalo <dmitry@dygalo.dev>
2023-08-20 09:02:23 +0200

    chore: rustfmt

--- css-inline/src/html/attributes.rs
+++ css-inline/src/html/attributes.rs
@@ -13,10 +13,10 @@ use std::{
 #[derive(Debug)]
 pub(crate) struct Class {
     pub(crate) value: StrTendril,
-    // In CSS selector matching, checking an element's class is frequent. 
-    // Given that classes are often specific, most elements won't have the checked class. 
-    // Leveraging this, we use a Bloom filter for a quick initial check. 
-    // If positive, we do an actual check. This two-tier approach ensures fewer actual checks on class attributes
+    /// In CSS selector matching, checking an element's class is frequent.
+    /// Given that classes are often specific, most elements won't have the checked class.
+    /// Leveraging this, we use a Bloom filter for a quick initial check.
+    /// If positive, we do an actual check. This two-tier approach ensures fewer actual checks on class attributes.
     pub(crate) cache: CountingBloomFilter<BloomStorageBool>,
 }
 
@@ -33,16 +33,16 @@ impl Class {
         Class { value, cache }
     }
 
-    // Check whether this element MAY have the given class.
-    // If positive, then the class is probably there, and in this case we need to perform the actual check.
-    // Otherwise, the element does not have this class.
+    /// Check whether this element MAY have the given class.
+    /// If positive, then the class is probably there, and in this case we need to perform the actual check.
+    /// Otherwise, the element does not have this class.
     #[inline]
     pub(crate) fn might_have_class(&self, name: &[u8]) -> bool {
         let hash = hash_class_name(name);
         self.cache.might_contain_hash(hash)
     }
 
-    // Manually check whether the class atteibute value contains the given class.
+    /// Manually check whether the class atteibute value contains the given class.
     #[inline]
     fn has_class_impl(&self, name: &[u8], case_sensitivity: CaseSensitivity) -> bool {
         for class in self.value.split(SELECTOR_WHITESPACE) {
