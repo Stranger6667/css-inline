2524eb9e8c04b954a23a95699f434868dcac8b0b
Dmitry Dygalo <dmitry@dygalo.dev>
2023-08-21 02:57:28 +0200

    perf: Avoid Bloom filter on single-class elements

--- css-inline/src/html/attributes.rs
+++ css-inline/src/html/attributes.rs
@@ -10,18 +10,14 @@ use std::{
 #[derive(Debug)]
 pub(crate) struct Class {
     pub(crate) value: StrTendril,
-    /// In CSS selector matching, checking an element's class is frequent. Given that classes are
-    /// often specific, most elements won't have the checked class. Leveraging this, we use a Bloom
-    /// filter for a quick initial check. If positive, we do an actual check. This two-tier
-    /// approach ensures fewer actual checks on class attributes.
-    pub(crate) cache: BloomFilter,
+    pub(crate) cache: Cache,
 }
 
 static SELECTOR_WHITESPACE: &[char] = &[' ', '\t', '\n', '\r', '\x0C'];
 
 /// This Bloom filter has 64 bits of storage and two hash functions which gives a decent false
 /// positive rate given that most of the time an element has a very few number of classes.
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub(crate) struct BloomFilter(u64);
 
 impl BloomFilter {
@@ -37,8 +33,17 @@ impl BloomFilter {
     }
 
     #[inline]
-    fn might_contain_hash(&self, hash: u64) -> bool {
+    fn might_contain_hash(self, hash: u64) -> bool {
         self.0 & (1 << (hash1(hash) % 64)) != 0 && self.0 & (1 << (hash2(hash) % 64)) != 0
+    }
+
+    /// Check whether this element MAY have the given class.
+    /// If positive, then the class is probably there, and in this case we need to perform the actual check.
+    /// Otherwise, the element does not have this class.
+    #[inline]
+    fn might_have_class(self, name: &[u8]) -> bool {
+        let hash = hash_class_name(name);
+        self.might_contain_hash(hash)
     }
 }
 
@@ -55,24 +60,42 @@ fn hash2(hash: u64) -> u64 {
     (hash >> KEY_SIZE) & KEY_MASK
 }
 
+#[derive(Debug)]
+pub(crate) enum Cache {
+    /// In CSS selector matching, checking an element's class is frequent. Given that classes are
+    /// often specific, most elements won't have the checked class. Leveraging this, we use a Bloom
+    /// filter for a quick initial check. If positive, we do an actual check. This two-tier
+    /// approach ensures fewer actual checks on class attributes.
+    Bloom(BloomFilter),
+    /// Element has a single class.
+    Single,
+}
+
 impl Class {
     fn new(value: StrTendril) -> Class {
         // Build a Bloom filter for all element's classes
         let mut cache = BloomFilter::new();
-        for class in value.split(SELECTOR_WHITESPACE) {
+        let mut classes = value.split(SELECTOR_WHITESPACE).filter(|s| !s.is_empty());
+        if let Some(class) = classes.next() {
+            // If the first class we split is the same as the input value, then we have just
+            // a single class and a Bloom filter is not needed.
+            if class.len() == value.len() {
+                return Class {
+                    value,
+                    cache: Cache::Single,
+                };
+            }
             let hash = hash_class_name(class.as_bytes());
             cache.insert_hash(hash);
         }
-        Class { value, cache }
-    }
-
-    /// Check whether this element MAY have the given class.
-    /// If positive, then the class is probably there, and in this case we need to perform the actual check.
-    /// Otherwise, the element does not have this class.
-    #[inline]
-    pub(crate) fn might_have_class(&self, name: &[u8]) -> bool {
-        let hash = hash_class_name(name);
-        self.cache.might_contain_hash(hash)
+        for class in classes {
+            let hash = hash_class_name(class.as_bytes());
+            cache.insert_hash(hash);
+        }
+        Class {
+            value,
+            cache: Cache::Bloom(cache),
+        }
     }
 
     /// Manually check whether the class attribute value contains the given class.
@@ -88,9 +111,10 @@ impl Class {
 
     #[inline]
     pub(crate) fn has_class(&self, name: &[u8], case_sensitivity: CaseSensitivity) -> bool {
-        match case_sensitivity {
-            CaseSensitivity::CaseSensitive => {
-                if self.might_have_class(name) {
+        match (&self.cache, case_sensitivity) {
+            (Cache::Single, case_sensitivity) => case_sensitivity.eq(self.value.as_bytes(), name),
+            (Cache::Bloom(bloom_filter), CaseSensitivity::CaseSensitive) => {
+                if bloom_filter.might_have_class(name) {
                     self.has_class_impl(name, case_sensitivity)
                 } else {
                     // Class is not in the Bloom filter, hence the this `class` value does not
@@ -98,7 +122,9 @@ impl Class {
                     false
                 }
             }
-            CaseSensitivity::AsciiCaseInsensitive => self.has_class_impl(name, case_sensitivity),
+            (Cache::Bloom(_), CaseSensitivity::AsciiCaseInsensitive) => {
+                self.has_class_impl(name, case_sensitivity)
+            }
         }
     }
 }
