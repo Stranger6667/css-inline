da112efc1b7854488e8937889d5adec833f3590b
Dmitry Dygalo <dmitry@dygalo.dev>
2023-12-28 02:18:31 +0100

    chore: Better network error message on WASM

--- bindings/javascript/src/errors.rs
+++ bindings/javascript/src/errors.rs
@@ -37,10 +37,25 @@ impl From<InlineError> for napi::Error {
 #[cfg(target_arch = "wasm32")]
 impl From<InlineError> for JsValue {
     fn from(error: InlineError) -> Self {
-        if let css_inline::InlineError::ParseError(e) = error.0 {
-            JsValue::from_str(&e)
-        } else {
-            JsValue::from_str(error.0.to_string().as_str())
+        if let css_inline::InlineError::IO(error) = &error.0 {
+            panic!("{}", error);
+        }
+        match &error.0 {
+            css_inline::InlineError::ParseError(e) => JsValue::from_str(e),
+            css_inline::InlineError::Network {
+                error: network_error,
+                ..
+            } => {
+                if let attohttpc::ErrorKind::Io(io_error) = network_error.kind() {
+                    if io_error.kind() == std::io::ErrorKind::Unsupported {
+                        return JsValue::from_str(
+                            "Loading remote stylesheets is not supported on WASM",
+                        );
+                    }
+                }
+                JsValue::from_str(error.0.to_string().as_str())
+            }
+            _ => JsValue::from_str(error.0.to_string().as_str()),
         }
     }
 }
