fda420e94cf8147cb839a93b11cfe1b627d313f8
Dmitry Dygalo <dmitry@dygalo.dev>
2024-12-27 13:05:33 +0100

    chore: Update PyO3 to 0.23
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -377,9 +377,10 @@ fn css_inline(py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
     module.add_wrapped(wrap_pyfunction!(inline_fragment))?;
     module.add_wrapped(wrap_pyfunction!(inline_many))?;
     module.add_wrapped(wrap_pyfunction!(inline_many_fragments))?;
-    let inline_error = py.get_type_bound::<InlineError>();
+    let inline_error = py.get_type::<InlineError>();
     inline_error.setattr("__doc__", INLINE_ERROR_DOCSTRING)?;
     module.add("InlineError", inline_error)?;
+    #[allow(deprecated)]
     module.add("__build__", pyo3_built::pyo3_built!(py, build))?;
     Ok(())
 }
